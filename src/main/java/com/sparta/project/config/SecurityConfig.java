e18fffa7017aa92acb325089d28ebd0d6efbc24c
kdy-git <zinee2001@naver.com>
2022-09-03 09:34:54 +0900

    SwaggerConfig 추가

--- src/main/java/com/sparta/project/config/SecurityConfig.java
+++ src/main/java/com/sparta/project/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/auth/**").permitAll()//해당 path는 인증없이 접근 허용한다.
                 .antMatchers(HttpMethod.OPTIONS, "/api/match").permitAll()
                 .antMatchers("/").permitAll()
-                .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
+                .anyRequest().permitAll()   // 나머지 API 는 전부 인증 필요
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
                 .and()
