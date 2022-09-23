9a705144fa4479a0fdf65862c0a5ea9b7fad64e7
kdy-git <zinee2001@naver.com>
2022-09-23 16:39:35 +0900

    #14 항상 200OK만 반환 되는것을 Exception별로 상태코드 다르게 나오도록 수정

--- src/main/java/com/sparta/project/errorHandler/ErrorHandler.java
+++ src/main/java/com/sparta/project/errorHandler/ErrorHandler.java
@@ -1,15 +1,32 @@
 package com.sparta.project.errorHandler;
 
+import com.amazonaws.services.kms.model.NotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.UnsupportedMediaTypeStatusException;
 
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler
-    public ResponseEntity<Object> ErrorMessage(Exception ex) {
-        return ResponseEntity.ok().body(ex.getMessage());
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<Object> ErrorMessageUserCheck(Exception ex) {
+        return ResponseEntity.status(401).body(ex.getMessage());
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> ErrorMessageServerCheck(Exception ex) {
+        return ResponseEntity.status(500).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> ErrorMessageMatchCheck(Exception ex) {
+        return ResponseEntity.status(404).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UnsupportedMediaTypeStatusException.class)
+    public ResponseEntity<Object> ErrorMessageImageCheck(Exception ex) {
+        return ResponseEntity.status(415).body(ex.getMessage());
+    }
 }
